a085ea89b9b334e1b78519180927d92ecc700d5e
NadiaErica <nadiaerica727@gmail.com>
2023-11-09 09:16:56 +0700

    ok

---
+++ src/skrip-utama.py
@@ -0,0 +1,9 @@
+from luas import lingkaran, persegi, segitiga
+print(lingkaran.luas_lingkaran(12))
+print(persegi.luas_persegi(6))
+print(segitiga.luas_segitiga(2,3))
+
+from volume import bola, kubus, tabung
+print(bola.volume_bola(18))
+print(kubus.volume_kubus(6))
+print(tabung.volume_tabung(2,8))
